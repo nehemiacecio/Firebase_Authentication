027464acf5faf5d891560c7577e628d694a56904
nehemiacecio <nehemia.cecio.3@gmail.com>
2021-12-02 04:44:00 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/umn/testingfirebase/DashboardActivity.kt
@@ -0,0 +1,34 @@
+package id.ac.umn.testingfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+
+        id_txt.text = currentUser?.uid
+        name_txt.text = currentUser?.displayName
+        email_txt.text = currentUser?.email
+
+        Glide.with(this).load(currentUser?.photoUrl).into(profile_image)
+
+        sign_out_btn.setOnClickListener {
+            mAuth.signOut()
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
